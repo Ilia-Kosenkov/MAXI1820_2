359903d879b390d0acd6bde08398107c4b2f30a5
Ilia Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2018-11-23 13:13:47 +0200

    Extended styles

--- Source/StyleProvider.R
+++ Source/StyleProvider.R
@@ -21,17 +21,24 @@
 # OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 purrr::pmap(tibble::tribble(
-    ~Vars,                 ~Vals,
-    "Style_LabelFontSz",   17,
-    "Style_TickFontSz",    15,
-    "Style_GroupColors",   brewer.pal(8, "Paired")[c(1, 5, 3, 9, 2, 6, 4, 10)],
-    "Style_GroupFills",    brewer.pal(8, "Paired")[c(1, 5, 3, 9, 2, 6, 4, 10)],
-    "Style_GroupShapes",   c(1, 0, 5, 2, 21, 22, 23, 24),
-    "Style_Groups",        0:7,
-    "Style_LabsMarStd",    margin(0.0, 0.0, 1, 1, "cm"),
-    "Style_AxisMarStd",    margin(0.25, 0.25, 0.5, 0.75, "cm"),
-    "Style_VGapStd",       unit(0.1, "cm"),
-    "Style_WidthStdInch",  6,
-    "Style_HeightStdInch", 6
+    ~Vars,                    ~Vals,
+    "Style_LabelFontSz",      17,
+    "Style_TickFontSz",       15,
+    "Style_GroupColors",      brewer.pal(8, "Paired")
+                                [c(1, 5, 3, 9, 2, 6, 4, 10)],
+    "Style_GroupFills",       brewer.pal(8, "Paired")
+                                [c(1, 5, 3, 9, 2, 6, 4, 10)],
+    "Style_GroupShapes",      c(1, 0, 5, 2, 21, 22, 23, 24),
+    "Style_Groups",           0:7,
+    "Style_GroupsAvgOnly",    c(4:7, 0:3),
+    "Style_GroupsBands",      2:4,
+    "Style_GroupLinesBands",  c(1, 2, 4),
+    "Style_GroupColorsBands", brewer.pal(3, "Set1")[c(2, 3, 1)],
+    "Style_GroupShapesBands", c(21, 22, 23, 24),
+    "Style_LabsMarStd",       margin(0.0, 0.0, 1, 1, "cm"),
+    "Style_AxisMarStd",       margin(0.25, 0.25, 0.5, 0.75, "cm"),
+    "Style_VGapStd",          unit(0.1, "cm"),
+    "Style_WidthStdInch",     6,
+    "Style_HeightStdInch",    6
     ),
     ~makeActiveBinding(.x, function() .y, .GlobalEnv))
