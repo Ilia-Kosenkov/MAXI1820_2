d3fd4bbe091526986f7e3d6d1daed17c00f285a7
Ilia <ilia.kosenkov.at.gm@gmail.com>
2020-01-24 16:42:10 +0200

    Different parsing approach

--- Source/Experimental/jet.R
+++ Source/Experimental/jet.R
@@ -1,15 +1,35 @@
 parse_jet_data <- function(path = fs::path("Input", "jet.txt")) {
     path %>%
-        read_table2(col_names = FALSE, col_types = cols()) %>%
+        read_table2(col_names = FALSE,
+            col_types = cols(
+                col_double(),
+                col_character(),
+                col_character(),
+                col_character(),
+                col_character(),
+                col_character(),
+                col_character(),
+                col_character(),
+                col_character(),
+                col_character(),
+                col_factor())) %>%
         set_names(cc(
             "MJD",
             "RA_cr", "DEC_cr", "?_cr",
             "RA_ap", "DEC_ap", "?_ap",
             "RA_rc", "DEC_rc", "?_rc",
             "Type")) %>%
-        mutate(Type = as_factor(Type)) %>%
         mutate_all(~na_if(., "-")) %>%
-        mutate_at(vars(starts_with("?")), ~if (vec_is(., character())) parse_double(.) else .)
+        mutate_at(vars(starts_with("?")), parse_double) %>%
+        mutate_at(vars(starts_with("RA")), enframe, NULL, "Src") %>%
+        mutate_at(
+            vars(starts_with("RA")),
+            ~ separate(.,
+                col = Src,
+                into = cc("H", "M", "S"), 
+                sep = ":",
+                remove = FALSE)) #%>%
+        #select(RA_cr)
 
 
 }
