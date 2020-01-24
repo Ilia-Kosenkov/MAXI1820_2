3bb660c42ad474d16ba4ac6f30323547fb1f1014
Ilia <ilia.kosenkov.at.gm@gmail.com>
2020-01-24 16:06:39 +0200

    Output as ascii

--- Source/Experimental/table_supplement.R
+++ Source/Experimental/table_supplement.R
@@ -14,17 +14,26 @@ generate_data_for_csv <- function(data = ReadAllAvgData(pattern = "pol_avg_(?<id
         transmute(
             MJD, Filter = fct_relevel(Band, "B", "V", "R"),
             Phase = fct_recode(Group, `RH` = "0", `HIM` = "1", `Soft` = "2", `DH` = "3"),
-            Q = Px, U = Py, P, PErr = SG, A, AErr = SG_A,
+            Q = Px, U = Py, P, e_P = SG, A, e_A = SG_A,
             Date = mjd_2_date(MJD)) %>%
         arrange(Filter, MJD) %>%
-        mutate_at(vars(Q, U, P, PErr), round, 3) %>%
-        mutate_at(vars(A, AErr), round, 2) %>%
+        mutate_at(vars(Q, U, P, e_P), round, 3) %>%
+        mutate_at(vars(A, e_A), round, 2) %>%
         mutate(MJD = round(MJD, 6))
+}
+
+print_as_fixed_width <- function(data, path = fs::path("Output", "supplementary.dat")) {
+    write_smart(data, path,
+                frmt = cc("%14.6f", "%8s", "%6s",
+                    vec_repeat("%8.3f", 4L),
+                    "%9.2f", "%8.2f",
+                    "%22s"))
 }
 
 if (get0("ShouldRun", ifnotfound = FALSE)) {
     path <- fs::path("Output", "supplementary.csv")
     generate_data_for_csv() %T>%
         print(n = len(.)) %>%
-        write_csv(path)
+        print_as_fixed_width
+        #write_csv(path)
 }
